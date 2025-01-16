5daf2b04e2a85167de46f55ca125f9656c80d955
Nika <mr.nika2020@mail.ru>
2025-01-16 20:09:59 +0400

    second commit

---
+++ level30/hw/6hw.py
@@ -0,0 +1,15 @@
+# 6) მომხმარებელს შემოატანინეთ 5 რიცხვი და დაამატეთ ეს რიცხვები სიაში, ბოლოს გამოიტანეთ ტერმინალში ეს სია 
+num1 = int(input("შემოიყვანე 1 რიცხვი: "))
+num2 = int(input("შემოიყვანე 2 რიცხვი: "))
+num3 = int(input("შემოიყვანე 3 რიცხვი: "))
+num4 = int(input("შემოიყვანე 4 რიცხვი: "))
+num5 = int(input("შემოიყვანე 5 რიცხვი: "))
+
+num_list=[]
+
+num_list.append(num1)
+num_list.append(num2)
+num_list.append(num3)
+num_list.append(num4)
+num_list.append(num5)
+print(num_list)
